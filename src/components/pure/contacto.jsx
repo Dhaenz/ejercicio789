2e1d781f428aee7df9829b4c070eab4192f2fe58
Dhaenz <ite.roberto.rossano@gmail.com>
2023-03-31 09:23:07 -0600

    dependencias bootstrao y componentes principales

---
+++ src/components/pure/contacto.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Contacto } from '../../models/contacto'
+
+function contacto() {
+  return (
+    <div>contacto</div>
+  )
+}
+
+export default contacto
---
+++ src/models/contacto.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+export class Contacto{
+    name = '';
+    celphone = '';
+    status = false;
+    email = '';
+
+    constructor(name, celphone, status, email){
+        this.name = name;
+        this.celphone = celphone;
+        this.status = status ;
+        this.email = email;
+    }
+}
+
+Contacto.propTypes = {
+    name: PropTypes.string,
+    description: PropTypes.number,
+    status: PropTypes.bool,
+    email: PropTypes.string
+
+}
